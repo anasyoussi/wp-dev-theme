d5f1b992b93bead8ef24281ac23a47b44b88f875
anasyoussi <18307320+anasyoussi@users.noreply.github.com>
2023-10-25 12:31:41 +0100

    Autoloader, Singleton, Aquila-class

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php 
+
+/**
+ * Footer Template.
+ * 
+ * @package Aquila
+ */  
+?>
+    <footer>Footer</footer>
+</div> <!-- ./id="page" class="site" -->
+</div> <!-- ./id="content" class="site-content" -->
+    <?php wp_footer(); ?>
+</body>
+</html>
